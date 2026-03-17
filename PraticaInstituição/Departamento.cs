58e3c5591275431cff129982513c6e4a085a6529
VictorSamu1 <victorsamuelizaias@gmail.com>
2026-03-16 21:42:38 -0300

    Adicionar arquivos de projeto.

---
+++ PraticaInstituição/Departamento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PraticaInstituição
+{
+   class Departamento
+    {
+        public string Nome {  get; set; }
+    }
+}
---
+++ PraticaInstituição/Instituicao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PraticaInstituição
+{
+  class Instituicao
+    {
+        public string Nome {  get; set; }
+        public string Endereco { get; set; }
+        public Departamento[] Departamentos
+        {  get;  } = new Departamento[10];
+
+        private int quantidadeDepartamentos = 0;
+
+        public void RegistrarDepartamento (Departamento d)
+        {
+            if (quantidadeDepartamentos < 10)
+                Departamentos[quantidadeDepartamentos++] = d;  
+
+        }
+
+        public int ObterQuantidadeDepartamentos()
+        {
+            return quantidadeDepartamentos;
+        }
+
+    }
+
+   
+
+
+}
